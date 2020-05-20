542740221579fb1feb3396eaf5ac2aa787ff84fb
atka007 <atka007@gmail.com>
2020-05-19 20:56:54 -0700

    Update test.py

--- test.py
+++ test.py
@@ -7,9 +7,10 @@ my_bot.sync_follows()
 
 #my_bot.auto_follow("cars")
 
-my_bot.auto_follow("CNET")
-my_bot.auto_follow_followers_of_user("CNET", count=100)
-
+#my_bot.auto_follow("CNET")
+my_bot.auto_follow_followers_of_user("CNET", count=20)
+my_bot.auto_follow_followers_of_user("wired", count=20)
+my_bot.auto_follow_followers_of_user("verge", count=20)
 #my_bot.auto_fav("phrase", count=1000)
 
 #my_bot.auto_rt("phrase", count=1000)
