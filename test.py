724ca3150cf01742fc82c2e5a02976a0a0eeee11
atka007 <atka007@gmail.com>
2020-05-19 21:46:12 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,8 @@ my_bot = TwitterBot()
 #my_bot.send_tweet("Hello world!")
 
 #Sync follows every day
-while True:
-  my_bot.sync_follows()
-  wait(86400)
 
+ my_bot.sync_follows()
 
 #my_bot.auto_follow("cars")
 
