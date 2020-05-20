c45a09c9a9491ccfd094f68da60e1bd9e99e02ac
atka007 <atka007@gmail.com>
2020-05-19 18:44:20 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,14 @@ my_bot = TwitterBot()
 my_bot.sync_follows()
 my_bot.send_tweet("Hello world!")
 
-my_bot.auto_follow("cars")
+#my_bot.auto_follow("cars")
+
+my_bot.auto_follow("CNET")
+my_bot.auto_follow_followers_of_user("CNET", count=100)
+
+#my_bot.auto_fav("phrase", count=1000)
+
+#my_bot.auto_rt("phrase", count=1000)
+
+#my_bot.auto_unfollow_nonfollowers()
+#my_bot.auto_unfollow_all_followers()
