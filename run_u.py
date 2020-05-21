4fa3d35ed888ff9be404c1c732f93df944cd38e0
atka007 <atka007@gmail.com>
2020-05-20 23:38:08 -0700

    Update run_u.py

--- run_u.py
+++ run_u.py
@@ -13,4 +13,5 @@ print("Running Unfollow Bot!")
 my_bot.auto_unfollow_all_followers(2)
   
 #30261067,18286505,18742444,893484744725467137,972651,18927441,14763734,816653,10876852,1344951,275686563,14372486,2890961
+
 print("Unfollow BOT Finished Process!")
