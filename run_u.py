6be7fd2985d9e02f345019cd29bfe3ec12c26825
atka007 <atka007@gmail.com>
2020-05-20 22:31:55 -0700

    Update run_u.py

--- run_u.py
+++ run_u.py
@@ -5,10 +5,10 @@ import random
 
 my_bot = TwitterBot()
 
-print("Running Bot!")
+print("Running Unfollow Bot!")
 
 #Unfolow people
-#my_bot.auto_unfollow_all_followers()
+my_bot.auto_unfollow_all_followers()
   
 #30261067,18286505,18742444,893484744725467137,972651,18927441,14763734,816653,10876852,1344951,275686563,14372486,2890961
-print("BOT Finished Process!")
+print("Unfollow BOT Finished Process!")
