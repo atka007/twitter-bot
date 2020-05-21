d4c2937b2dc2a4ef9ceb7af7ca8e0d8fa087e217
atka007 <atka007@gmail.com>
2020-05-20 22:12:04 -0700

    Update test.py

--- test.py
+++ test.py
@@ -26,9 +26,9 @@ for x in range(3):
   time.sleep(wait_time)
   print("Actions: " + str(actions))
 
-  #my_bot.auto_follow_followers_of_user("CNET", count=int(ceil(actions)))
+  my_bot.auto_follow_followers_of_user("CNET", count=int(ceil(actions)))
   
-  #my_bot.auto_fav("luxury", count=2)
+  my_bot.auto_fav("luxury", count=2)
   
   #my_bot.auto_follow_followers_of_user("wired", count=int(ceil(actions)))
   
