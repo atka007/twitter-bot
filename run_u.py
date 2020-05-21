d7ce8fe057e644a3419c0493e499baca97201ab0
atka007 <atka007@gmail.com>
2020-05-20 22:29:48 -0700

    Create run_u.py

---
+++ run_u.py
@@ -0,0 +1,14 @@
+from TwitterFollowBot import TwitterBot
+import time
+import math
+import random
+
+my_bot = TwitterBot()
+
+print("Running Bot!")
+
+#Unfolow people
+#my_bot.auto_unfollow_all_followers()
+  
+#30261067,18286505,18742444,893484744725467137,972651,18927441,14763734,816653,10876852,1344951,275686563,14372486,2890961
+print("BOT Finished Process!")
