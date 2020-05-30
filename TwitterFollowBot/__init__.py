ebaf8d29639d99c2a6c1318a50daf6715f1a8483
atka007 <atka007@gmail.com>
2020-05-29 17:11:00 -0700

    Update __init__.py

--- TwitterFollowBot/__init__.py
+++ TwitterFollowBot/__init__.py
@@ -485,8 +485,9 @@ class TwitterBot:
 					#self.wait_on_action()
                     
                     # sends dm
-                    username = self.TWITTER_CONNECTION.get_user(user_id).screen_name  #not working
-                    self.TWITTER_CONNECTION.send_direct_message(user_id=user_id, text='{} {},\n{}'.format(greeting, username, message))
+                    #username = self.TWITTER_CONNECTION.get_user(user_id).screen_name  #not working
+                    #self.TWITTER_CONNECTION.send_direct_message(user_id=user_id, text='{} {},\n{}'.format(greeting, username, message))
+                    self.TWITTER_CONNECTION.send_direct_message(user_id=user_id, text='{}'.format(message))
                     #total_followed += 1
                     #if total_followed % 5 == 0:
                     #   print(str(total_followed) + ' messages sent so far.')
